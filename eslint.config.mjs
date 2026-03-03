816abf7ca203a3769689aba713100c4cf4ce813c
John Rummell <john@elitebusinesssolutions.com>
2026-03-03 09:57:29 -0500

    Chapter 14 - Adding Authentication

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  { rules: { curly: ["error", "all"] } },
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"])
 ]);
 
